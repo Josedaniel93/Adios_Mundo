fc2fe6ba39d5f19fad7c4e7eb5d5eee66acc8e7f
Luciano Barrera <lucianobarrerapaez@gmail.com>
2019-10-03 18:27:51 +0200

    Experimentos con colores 2

--- AdiosMundo.java
+++ AdiosMundo.java
@@ -5,9 +5,10 @@ public class AdiosMundo {
 		// TODO Auto-generated method stub
 		String amarillo = "\033[33m";
 		String rojo = "\033[31m";
+		String reset = "\033[0m";
 
-		System.out.println(amarillo+"Adios Mundo");
-		System.out.println(rojo+"Pero volveré");
+		System.out.println(amarillo+"Adios Mundo"+reset);
+		System.out.println(rojo+"Pero volveré"+reset);
 
 	}
 
